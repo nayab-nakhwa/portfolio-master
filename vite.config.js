d04c82b8cccf07073d1e0b0d13ebe4694b99b38c
nayab-nakhwa <nayabnakhwa1@gmail.com>
2024-08-11 17:04:52 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ghPages } from 'vite-plugin-gh-pages';
 
 // vite.config.js
 import { qrcode } from 'vite-plugin-qrcode';
@@ -8,5 +9,5 @@ import { qrcode } from 'vite-plugin-qrcode';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), qrcode()],
-  base: "/vite-deploy/"
+  base: '/portfolio-master/',
 })
