895b8a4b9a65ef5f5d4bc3eb082c0000878c3a93
nayab-nakhwa <nayabnakhwa1@gmail.com>
2024-09-20 20:12:31 +0530

    Fixed Footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import {
-  FaFacebook,
-  FaTwitter,
-  FaInstagram,
-  FaLinkedinIn,
-} from "react-icons/fa6";
+import { FaInstagram, FaSquareFacebook } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+import { FaTelegram } from "react-icons/fa6";
+
 function Footer() {
   return (
     <>
@@ -13,10 +11,37 @@ function Footer() {
         <div className="max-w-screen-2xl container mx-auto px-4 md:px-20">
           <div className=" flex flex-col items-center justify-center">
             <div className="flex space-x-4">
-              <FaFacebook size={24} />
-              <FaTwitter size={24} />
-              <FaInstagram size={24} />
-              <FaLinkedinIn size={24} />
+            <ul className="flex space-x-5">
+                  <li>
+                    <a
+                      href="https://www.facebook.com/nayab.nakhwa/"
+                      target="_blank"
+                    >
+                      <FaSquareFacebook className="text-3xl cursor-pointer  hover:text-purple-700 hover:scale-125 duration-200" />
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://www.linkedin.com/in/nayabnakhwa/"
+                      target="_blank"
+                    >
+                      <FaLinkedin className="text-3xl cursor-pointer  hover:text-purple-700 hover:scale-125 duration-200" />
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://www.instagram.com/nayab_nakhwa"
+                      target="_blank"
+                    >
+                      <FaInstagram className="text-3xl cursor-pointer  hover:text-purple-700 hover:scale-125 duration-200" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://t.me/nayab_nakhwa" target="_blank">
+                      <FaTelegram className="text-3xl cursor-pointer  hover:text-purple-700 hover:scale-125 duration-200" />
+                    </a>
+                  </li>
+                </ul>
             </div>
             <div className="mt-8 border-t border-gray-700 pt-8 flex flex-col items-center">
               <p className="text-sm">
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,7 +13,7 @@ import { FaReact } from "react-icons/fa";
 import { FaNodeJs } from "react-icons/fa6";
 
 import { ReactTyped } from "react-typed";
-import './Home.css'
+import "./Home.css";
 
 function Home() {
   return (
@@ -37,46 +37,61 @@ function Home() {
             </div>
             <br />
             <p className="text-sm md:text-md text-justify">
-            Enthusiastic and detail-oriented Computer Science graduate with hands-on experience in full-stack web development. As a highly skilled Full Stack Web Developer, I offer a diverse skill set in HTML, CSS, JavaScript, MongoDB, SQL, Reactjs, Nodejs, PHP and Wordpress. With a strong background in developing and maintaining dynamic websites and applications.
+              Enthusiastic and detail-oriented Computer Science graduate with
+              hands-on experience in full-stack web development. As a highly
+              skilled Full Stack Web Developer, I offer a diverse skill set in
+              HTML, CSS, JavaScript, MongoDB, SQL, Reactjs, Nodejs, PHP and
+              Wordpress. With a strong background in developing and maintaining
+              dynamic websites and applications.
             </p>
             <br />
             <div className="flex flex-col space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4">
-            <a href="Nayab-resume.pdf" target="_blank">
-<button class="animated-button">
-  <svg viewBox="0 0 24 24" class="arr-2" xmlns="http://www.w3.org/2000/svg">
-    <path
-      d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
-    ></path>
-  </svg>
-  <span class="text">Hire Me!</span>
-  <span class="circle"></span>
-  <svg viewBox="0 0 24 24" class="arr-1" xmlns="http://www.w3.org/2000/svg">
-    <path
-      d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
-    ></path>
-  </svg>
-</button>
-</a>
-            <Link to='Contact'
-                    smooth={true}
-                    duration={500}
-                    offset={-70}
-                    activeClass="active">
-<button class="animated-button">
-  <svg viewBox="0 0 24 24" class="arr-2" xmlns="http://www.w3.org/2000/svg">
-    <path
-      d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
-    ></path>
-  </svg>
-  <span class="text">Contact Me?</span>
-  <span class="circle"></span>
-  <svg viewBox="0 0 24 24" class="arr-1" xmlns="http://www.w3.org/2000/svg">
-    <path
-      d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
-    ></path>
-  </svg>
-</button>
- </Link>
+              <a href="Nayab-resume.pdf" target="_blank">
+                <button class="animated-button">
+                  <svg
+                    viewBox="0 0 24 24"
+                    class="arr-2"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
+                  </svg>
+                  <span class="text">Hire Me!</span>
+                  <span class="circle"></span>
+                  <svg
+                    viewBox="0 0 24 24"
+                    class="arr-1"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
+                  </svg>
+                </button>
+              </a>
+              <Link
+                to="Contact"
+                smooth={true}
+                duration={500}
+                offset={-70}
+                activeClass="active"
+              >
+                <button class="animated-button">
+                  <svg
+                    viewBox="0 0 24 24"
+                    class="arr-2"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
+                  </svg>
+                  <span class="text">Contact Me?</span>
+                  <span class="circle"></span>
+                  <svg
+                    viewBox="0 0 24 24"
+                    class="arr-1"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
+                  </svg>
+                </button>
+              </Link>
             </div>
             <br />
             {/* social media icons */}
@@ -85,17 +100,26 @@ function Home() {
                 <h1 className="font-bold text-center ">Available on</h1>
                 <ul className="flex space-x-5">
                   <li>
-                    <a href="https://www.facebook.com/nayab.nakhwa/" target="_blank">
+                    <a
+                      href="https://www.facebook.com/nayab.nakhwa/"
+                      target="_blank"
+                    >
                       <FaSquareFacebook className="text-2xl cursor-pointer  hover:text-purple-700 hover:scale-125 duration-200" />
                     </a>
                   </li>
                   <li>
-                    <a href="https://www.linkedin.com/in/nayabnakhwa/" target="_blank">
+                    <a
+                      href="https://www.linkedin.com/in/nayabnakhwa/"
+                      target="_blank"
+                    >
                       <FaLinkedin className="text-2xl cursor-pointer  hover:text-purple-700 hover:scale-125 duration-200" />
                     </a>
                   </li>
                   <li>
-                    <a href="https://www.instagram.com/nayab_nakhwa" target="_blank">
+                    <a
+                      href="https://www.instagram.com/nayab_nakhwa"
+                      target="_blank"
+                    >
                       <FaInstagram className="text-2xl cursor-pointer  hover:text-purple-700 hover:scale-125 duration-200" />
                     </a>
                   </li>
@@ -109,10 +133,22 @@ function Home() {
               <div className=" space-y-2">
                 <h1 className="font-bold text-center">Currently working on</h1>
                 <div className="flex space-x-5">
-                 <a href="https://www.mongodb.com/" target="_blank"> <SiMongodb className="text-2xl md:text-3xl hover:scale-125 hover:text-purple-700 duration-200 rounded-full border-[2px] cursor-pointer" /></a>
-                 <a href="https://expressjs.com/" target="_blank"> <SiExpress className="text-2xl md:text-3xl hover:scale-125 hover:text-purple-700 duration-200 rounded-full border-[2px] cursor-pointer" /></a>
-                 <a href="https://react.dev/" target="_blank"> <FaReact className="text-2xl md:text-3xl hover:scale-125 hover:text-purple-700 duration-200 rounded-full border-[2px] cursor-pointer" /></a>
-                 <a href="https://nodejs.org/en" target="_blank"> <FaNodeJs className="text-2xl md:text-3xl hover:scale-125  hover:text-purple-700 duration-200 rounded-full border-[2px] cursor-pointer" /></a>
+                  <a href="https://www.mongodb.com/" target="_blank">
+                    {" "}
+                    <SiMongodb className="text-2xl md:text-3xl hover:scale-125 hover:text-purple-700 duration-200 rounded-full border-[2px] cursor-pointer" />
+                  </a>
+                  <a href="https://expressjs.com/" target="_blank">
+                    {" "}
+                    <SiExpress className="text-2xl md:text-3xl hover:scale-125 hover:text-purple-700 duration-200 rounded-full border-[2px] cursor-pointer" />
+                  </a>
+                  <a href="https://react.dev/" target="_blank">
+                    {" "}
+                    <FaReact className="text-2xl md:text-3xl hover:scale-125 hover:text-purple-700 duration-200 rounded-full border-[2px] cursor-pointer" />
+                  </a>
+                  <a href="https://nodejs.org/en" target="_blank">
+                    {" "}
+                    <FaNodeJs className="text-2xl md:text-3xl hover:scale-125  hover:text-purple-700 duration-200 rounded-full border-[2px] cursor-pointer" />
+                  </a>
                 </div>
               </div>
             </div>
